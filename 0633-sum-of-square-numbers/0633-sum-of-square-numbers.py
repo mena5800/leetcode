dc69869eee43b0f85edce7d98b49192c6c9c06ef
mena5800 <81194918+mena5800@users.noreply.github.com>
2022-12-21 13:00:00 +0200

    Time: 555 ms (44.29%), Space: 18.4 MB (5.15%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -0,0 +1,17 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        list_ = []
+        i = 0
+        while i*i <=c:
+            list_.append(i*i)
+            i +=1
+        
+        # there is two numbers equal to c
+        print(list_)
+        set_ = set(list_)
+        for i in range(len(list_)):
+            target = c - list_[i]
+            if target in set_:
+                return True
+        return False
+        
