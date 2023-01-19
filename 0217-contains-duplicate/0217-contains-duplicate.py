a1004038423c40b0af01cfa8e6714797e0660a9b
Mina Safwat <81194918+mena5800@users.noreply.github.com>
2023-01-19 14:48:14 +0200

    Time: 548 ms (41.58%), Space: 28.8 MB (26.72%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if len(nums) == len(set(nums)):
+            return False
+        else:
+            return True
+            
+            
+            
+        
+        
