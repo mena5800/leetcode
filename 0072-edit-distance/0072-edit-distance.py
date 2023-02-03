bb4258fa02c315f69f18d1356348371aab01c4bc
Mina Safwat <81194918+mena5800@users.noreply.github.com>
2023-02-03 03:58:52 +0200

    Time: 202 ms (38.91%), Space: 17.7 MB (30.70%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,21 +1,18 @@
-import numpy as np
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        mat = np.array([[0] * (len(word1)+1)]*(len(word2)+1))
-        for i in range(len(mat)):
-            for j in range(len(mat[0])):
-                if i == 0:
-                    mat[i][j] = j
-                if j == 0:
-                    mat[i][j] = i
+        m = len(word1) + 1
+        n = len(word2) + 1
+        dp = [[0 for x in range(n)] for x in range(m)]
 
-        for i in range(1, len(mat)):
-            for j in range(1, len(mat[0])):
-                score1 = mat[i-1][j] + 1
-                score2 = mat[i][j-1] + 1
-                if word1[j-1] == word2[i-1]:
-                    score3 = mat[i-1][j-1]
+        for i in range(m):
+            for j in range(n):
+                if i == 0:
+                    dp[i][j] = j
+                elif j == 0:
+                    dp[i][j] = i
+                elif word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
                 else:
-                    score3 = mat[i-1][j-1] + 1
-                mat[i][j] = min(score1, score2, score3)
-        return mat[-1][-1]
+                    dp[i][j] = 1 + min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1])
+
+        return dp[m-1][n-1]
